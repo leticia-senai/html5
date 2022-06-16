57f9e5db664ccfddc0fc2d25f2998bb8ee3bba6c
leticia-senai <0000882658@senaimgaluno.com.br>
2022-06-16 17:27:45 -0300

    x

--- gerador de senha/script.js
+++ gerador de senha/script.js
@@ -1,33 +1,32 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+let sliderElement=document.querySelector("#slider");
+let buttonElement=document.querySelector("#button");
 
-let sizePassword = document.querySelector("#valor");
-let passoword = document.querySelector("#password");
+let sizePassword=document.querySelector("#valor");
+let passoword= document.querySelector("#password");
 
-let containerPassword = document.querySelector("#container-passoword");
+let containerPassword= document.querySelector("#container-password");
+let charset ="abcdefghijklmnopqrstuvxwyABCDEFGHIJKLMNOPQRSTUVXZWY123466789!@#";
+let novaSenha = "";
 
-
-let charset ="abcdefghijklmnopqrstuvxzwykABCDEFGHIJKLMNOPQRSTUVXZWY0123456789!@";
-let novaSenha= "";
-
-sizePassword.innerHTML = sliderElement.value;
-
-slider.oninput = function (){
-    sizePassword.innerHTML = this.value;
+sizePassword.innerHTML=sliderElement.value;
+slider.oninput=function(){
+    sizePassword.innerHTML= this.value;
 }
+
 
 function generatePassoword(){
     let pass ="";
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random()* n));
     }
-    containerPassword.classList.remove("hide");
+containerPassword.classList.remove("hide");
     passoword.innerHTML =pass;
-    novaSenha = pass;
+ novaSenha =pass;
 }
+
 function copyPassword(){
-    alert ("Senha copiada com sucesso")
-    navigator.clipboard.writeText(novaSenha);
+    alert("Senha copiada com sucesso");
+   navigator.clipboard.writeText(novaSenha);
 }
     
 
